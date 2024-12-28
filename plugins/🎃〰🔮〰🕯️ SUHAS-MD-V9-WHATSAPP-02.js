c0d0a10d5fdb37773bea74c0816ecc3538aa2467
koli2006 <kolithanew123@gmail.com>
2024-12-28 17:06:06 +0530

    Update 🎃〰🔮〰🕯️ SUHAS-MD-V9-WHATSAPP-02.js

--- plugins/🎃〰🔮〰🕯️ SUHAS-MD-V9-WHATSAPP-02.js
+++ plugins/🎃〰🔮〰🕯️ SUHAS-MD-V9-WHATSAPP-02.js
@@ -5,1036 +5,174 @@
 // ░╚═══██╗██║░░░██║██╔══██║██╔══██║░╚═══██╗╚════╝██║╚██╔╝██║██║░░██║╚════╝░╚████╔╝░░╚═══██║
 // ██████╔╝╚██████╔╝██║░░██║██║░░██║██████╔╝░░░░░░██║░╚═╝░██║██████╔╝░░░░░░░░╚██╔╝░░░█████╔╝
 // ╚═════╝░░╚═════╝░╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░░░░░░░╚═╝░░░░░╚═╝╚═════╝░░░░░░░░░░╚═╝░░░░╚════╝░
-
-
 // * Project name - SUHAS-MD-V9 
 // * Author - Suhas Pathsindu.
 // * Team - 🇦🇴★ ͟͞  ͟͞ ̶❮ᓩＣ͔́ʏʙ͕֝́́ᴇʀ〴Ｄ֝֩ʀᴀ֞ͨͬͥɢᴏɴךＯ͕ͩꜰ͑́ꜰɪᴄ͚ͫɪᴀֹ֣֝ʟ❯ ͟͞ ̶ ͟͞★🇦🇴 
 // * Version - 9.0.0
-
 // World Best & Powerfull WhatsApp User Bot in Sri Lanka.
 // Don't Copy My Codes. :(
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const _0x577879=_0xb6c4;(function(_0x461b5f,_0x2433cb){const _0x4afb88=_0xb6c4,_0x567b3e=_0x461b5f();while(!![]){try{const _0x3e9b3b=-parseInt(_0x4afb88(0xe5))/0x1+-parseInt(_0x4afb88(0xfc))/0x2+parseInt(_0x4afb88(0xec))/0x3+parseInt(_0x4afb88(0x106))/0x4*(-parseInt(_0x4afb88(0xfd))/0x5)+-parseInt(_0x4afb88(0x108))/0x6+parseInt(_0x4afb88(0xd8))/0x7*(parseInt(_0x4afb88(0xd5))/0x8)+parseInt(_0x4afb88(0xcf))/0x9;if(_0x3e9b3b===_0x2433cb)break;else _0x567b3e['push'](_0x567b3e['shift']());}catch(_0x335a5a){_0x567b3e['push'](_0x567b3e['shift']());}}}(_0x145c,0xc5b39));function _0x145c(){const _0x1a0802=['tr:nth-child(1)\x20>\x20td:nth-child(3)','\x0a*📆\x20Yᴇᴀʀ\x20:*\x20','886764VqjPFq','tr:nth-child(3)\x20>\x20td:nth-child(1)\x20>\x20a','tr:nth-child(1)\x20>\x20td:nth-child(1)\x20>\x20a','find','*❗️\x20Mᴏᴠɪᴇ\x20Nᴏᴛ\x20Fᴏᴜɴᴅ.\x20❗️*','#contenedor\x20>\x20div.module\x20>\x20div.content.rigth.csearch\x20>\x20div\x20>\x20div.no-result.animation-2\x20>\x20h2','#repimdb\x20>\x20strong','4125531JVtLiD',')\x20:*\x0a','\x0a\x0a⭐\x20*IMDB\x20Rᴀᴛᴇ\x20:*\x20','load','.year','\x0a\x0a>\x20*✨\x20Powered\x20By\x20SUHAS-MD-V9\x20💞*','tr:nth-child(2)\x20>\x20td:nth-child(1)\x20>\x20a','year','\x0a*⭐️\x20Rᴀᴛɪɴɢ\x20:*\x20','\x0a\x0a*3️⃣\x20480P(','sinhalasublk','article','react','cineinfo','title','get','2196784zjzGQd','63355wpTzFO','tr:nth-child(2)\x20>\x20td:nth-child(3)','\x0a\x0a🤵‍♂\x20*Dɪʀᴇᴄᴛᴏʀ:*\x20','\x0a*🔗\x20Uʀʟ\x20:*\x20','https://cinesubz.co/?s=','search','src','tr:nth-child(3)\x20>\x20td:nth-child(3)','\x0a\x0a⏱\x20*Dᴜʀᴀᴛɪᴏɴ\x20:*\x20','176jFNShV','.title\x20a','8688168gYQTCt','\x0a\x0a🌍\x20*Cᴏᴜɴᴛʀʏ\x20:*\x20','*\x0a\x0a🧿\x20*Rᴇʟᴇᴀꜱᴇ\x20Dᴀᴛᴇ\x20:*\x20','attr','link','23178528AopvNC','sendMessage','\x0a*📝\x20Tɪᴛʟᴇ\x20:*\x20','#single\x20>\x20div.content.right\x20>\x20div.sheader\x20>\x20div.poster\x20>\x20img','text','axios','6799392bgVnNe','push','\x0a\x0a*4️⃣\x20Sɪɴʜᴀʟᴀ\x20Sᴜʙᴛɪᴛʟᴇᴀ\x20:*\x0a\x0a>\x20*✨\x20Powered\x20By\x20SUHAS-MD-V9\x20💞*','7DZgfVS','log','tbody:nth-child(2)\x20>\x20tr:nth-child(1)\x20>\x20td:nth-child(1)\x20>\x20a','data','href','\x0a\x0a*2️⃣\x20720P(','each','#single\x20>\x20div.content.right\x20>\x20div.sheader\x20>\x20div.data\x20>\x20div.extra\x20>\x20span.country','split','#contenedor\x20>\x20div.module\x20>\x20div.content.rigth.csearch\x20>\x20div\x20>\x20div.no-result.animation-2\x20>\x20h2\x20>\x20span','No\x20results\x20to\x20show\x20with\x20*'];_0x145c=function(){return _0x1a0802;};return _0x145c();}function _0xb6c4(_0x587e65,_0x2ef58d){const _0x145c7e=_0x145c();return _0xb6c4=function(_0xb6c487,_0x3bef94){_0xb6c487=_0xb6c487-0xce;let _0x2fa4f1=_0x145c7e[_0xb6c487];return _0x2fa4f1;},_0xb6c4(_0x587e65,_0x2ef58d);}const {cmd,commands}=require('../command'),axios=require(_0x577879(0xd4)),cheerio=require('cheerio');cmd({'pattern':_0x577879(0xf9),'desc':'cinesubz.co\x20info','category':_0x577879(0x102),'filename':__filename},async(_0x4b868b,_0x27181f,_0x3d2b26,{from:_0x30cfa2,quoted:_0x291386,body:_0x4a0a1,isCmd:_0x14e44a,command:_0x348bb9,args:_0x1f5bb3,q:_0xbe85b9,isGroup:_0x11e20b,sender:_0x577c3d,senderNumber:_0x55eee4,botNumber2:_0x202ce5,botNumber:_0x378d91,pushname:_0x2db482,isMe:_0x1e9e0e,isOwner:_0x1185e1,groupMetadata:_0x339a00,groupName:_0x4241ee,participants:_0x415914,groupAdmins:_0x1992ad,isBotAdmins:_0x5dfa80,isAdmins:_0x547368,reply:_0x2d01f2})=>{const _0x1f77e7=_0x577879;try{if(!_0xbe85b9)return _0x2d01f2('*Please\x20give\x20me\x20a\x20movie\x20name.‼*');let _0x58d0cb=await axios[_0x1f77e7(0xfb)](_0x1f77e7(0x101)+_0xbe85b9),_0x46ed5b=cheerio[_0x1f77e7(0xef)](_0x58d0cb[_0x1f77e7(0xdb)]),_0x381e26=_0x46ed5b('#contenedor\x20>\x20div.module\x20>\x20div.content.rigth.csearch\x20>\x20div\x20>\x20div:nth-child(2)\x20>\x20article\x20>\x20div.details\x20>\x20div.title\x20>\x20a')['attr'](_0x1f77e7(0xdc));if(!_0x381e26){let _0x2983f2=_0x46ed5b(_0x1f77e7(0xe1))[_0x1f77e7(0xd3)]();return _0x2d01f2(_0x1f77e7(0xe2)+_0x2983f2+'*');}let _0x2be399=await axios[_0x1f77e7(0xfb)](''+_0x381e26);_0x46ed5b=cheerio[_0x1f77e7(0xef)](_0x2be399[_0x1f77e7(0xdb)]);const _0x53e791=_0x46ed5b('#single\x20>\x20div.content.right\x20>\x20div.sheader\x20>\x20div.data\x20>\x20h1')[_0x1f77e7(0xd3)](),_0x4059b4=_0x46ed5b('#single\x20>\x20div.content.right\x20>\x20div.sheader\x20>\x20div.data\x20>\x20div.extra\x20>\x20span.date')[_0x1f77e7(0xd3)](),_0x565f1e=_0x46ed5b(_0x1f77e7(0xdf))[_0x1f77e7(0xd3)](),_0x3f9ec6=_0x46ed5b('#single\x20>\x20div.content.right\x20>\x20div.sheader\x20>\x20div.data\x20>\x20div.extra\x20>\x20span.runtime')[_0x1f77e7(0xd3)](),_0x5da131=_0x46ed5b(_0x1f77e7(0xeb))[_0x1f77e7(0xd3)](),_0x1ff39=_0x46ed5b('#cast\x20>\x20div:nth-child(2)\x20>\x20div\x20>\x20div.data\x20>\x20div.name\x20>\x20a')[_0x1f77e7(0xd3)](),_0x2a9b6e=_0x46ed5b(_0x1f77e7(0xd2))[_0x1f77e7(0x10b)](_0x1f77e7(0x103));let _0x4b39b2='🍟\x20*'+_0x53e791+_0x1f77e7(0x10a)+_0x4059b4+_0x1f77e7(0x109)+_0x565f1e+_0x1f77e7(0x105)+_0x3f9ec6+_0x1f77e7(0xee)+_0x5da131+_0x1f77e7(0xff)+_0x1ff39+'\x0a\x0a🖇\x20*Lɪɴᴋ\x20:*\x20'+_0x381e26+_0x1f77e7(0xf1);await _0x4b868b[_0x1f77e7(0xd0)](_0x30cfa2,{'image':{'url':_0x2a9b6e},'caption':_0x4b39b2},{'quoted':_0x27181f});}catch(_0x401b65){console['log'](_0x401b65),_0x2d01f2(''+_0x401b65);}}),cmd({'pattern':_0x577879(0xf6),'alias':[_0x577879(0xf9)],'desc':'Search\x20movies\x20in\x20sinhalasub.lk.','category':'search','filename':__filename},async(_0x1e4285,_0x59764,_0x3d0715,{from:_0x490774,quoted:_0x24942a,body:_0x3a0b67,isCmd:_0x373899,command:_0x272331,args:_0x116239,q:_0x40aea5,isGroup:_0x5567ef,sender:_0x43728b,senderNumber:_0x3a2f92,botNumber2:_0x4a93b5,botNumber:_0x5381ce,pushname:_0x599c57,isMe:_0x1c7961,isOwner:_0x39356b,groupMetadata:_0x1eb995,groupName:_0x684a6d,participants:_0x275be9,groupAdmins:_0x48a970,isBotAdmins:_0x5287e6,isAdmins:_0x2b0af0,reply:_0x5968f1})=>{const _0x13b319=_0x577879;try{const _0x3ad581='https://sinhalasub.lk/?s='+_0x40aea5,_0x2b0db0=await axios[_0x13b319(0xfb)](_0x3ad581),_0x1462ac=cheerio[_0x13b319(0xef)](_0x2b0db0[_0x13b319(0xdb)]),_0x4b42b6=_0x1462ac(_0x13b319(0xea))[_0x13b319(0xd3)]();if(_0x4b42b6=='No\x20results\x20to\x20show\x20with\x20'+_0x40aea5)return await _0x3d0715[_0x13b319(0xf8)]('❌'),_0x5968f1(_0x13b319(0xe9));await _0x3d0715[_0x13b319(0xf8)]('🎬');const _0x5378f0=[],_0x47c8d4=_0x1462ac(_0x13b319(0xf7));_0x47c8d4[_0x13b319(0xde)](function(){const _0x117d20=_0x13b319,_0x35d9d3=_0x1462ac(this)[_0x117d20(0xe8)](_0x117d20(0x107))[_0x117d20(0xd3)](),_0x5c5726=_0x1462ac(this)[_0x117d20(0xe8)]('.rating')[_0x117d20(0xd3)](),_0x2b88df=_0x1462ac(this)[_0x117d20(0xe8)](_0x117d20(0xf0))[_0x117d20(0xd3)](),_0x7fe78d=_0x1462ac(this)[_0x117d20(0xe8)](_0x117d20(0x107))['attr'](_0x117d20(0xdc));_0x5378f0[_0x117d20(0xd6)]({'title':_0x35d9d3,'rating':_0x5c5726,'year':_0x2b88df,'link':_0x7fe78d});});const _0x47c74b=_0x5378f0[0x0][_0x13b319(0xce)],_0x5468d6=await axios[_0x13b319(0xfb)](_0x47c74b),_0x5ed00f=cheerio[_0x13b319(0xef)](_0x5468d6[_0x13b319(0xdb)]),_0x269df9=_0x5ed00f(_0x13b319(0xe7))['attr']('href'),_0x5bc606=_0x5ed00f(_0x13b319(0xe3))['text'](),_0x11a967=_0x5bc606['split']('\x20')[0x0],_0x3a8d5f=_0x5bc606['search']('MB'),_0x343e78=_0x5ed00f(_0x13b319(0xf2))[_0x13b319(0x10b)](_0x13b319(0xdc)),_0x47dd11=_0x5ed00f(_0x13b319(0xfe))[_0x13b319(0xd3)](),_0xcb2c73=_0x47dd11[_0x13b319(0xe0)]('\x20')[0x0],_0x5694ce=_0x47dd11[_0x13b319(0x102)]('MB'),_0x84a4d4=_0x5ed00f(_0x13b319(0xe6))['attr'](_0x13b319(0xdc)),_0xb675bd=_0x5ed00f(_0x13b319(0x104))[_0x13b319(0xd3)](),_0xaf6037=_0xb675bd[_0x13b319(0xe0)]('\x20')[0x0],_0x1b4d77=_0xb675bd['search']('MB'),_0x13345b=_0x5ed00f(_0x13b319(0xda))[_0x13b319(0x10b)]('href'),_0x2fa577=_0x5ed00f(_0x13b319(0xd2))[_0x13b319(0x10b)](_0x13b319(0x103));let _0x18b33c='MB',_0x3e8459='MB',_0x30d3f2='MB';if(_0x3a8d5f==-0x1)_0x18b33c='GB';if(_0x5694ce==-0x1)_0x3e8459='GB';if(_0x1b4d77==-0x1)_0x30d3f2='GB';let _0x5b883a=_0x13b319(0xd1)+_0x5378f0[0x0][_0x13b319(0xfa)]+_0x13b319(0xf4)+_0x5378f0[0x0]['rating']+_0x13b319(0xe4)+_0x5378f0[0x0][_0x13b319(0xf3)]+_0x13b319(0x100)+_0x5378f0[0x0][_0x13b319(0xce)]+'\x0a\x0a*📥\x20Download\x20Links\x20📥*\x0a*1️⃣\x201080P('+_0x11a967+_0x18b33c+_0x13b319(0xed)+_0x269df9+_0x13b319(0xdd)+_0xcb2c73+_0x3e8459+_0x13b319(0xed)+_0x343e78+_0x13b319(0xf5)+_0xaf6037+_0x30d3f2+_0x13b319(0xed)+_0x84a4d4+_0x13b319(0xd7);await _0x1e4285[_0x13b319(0xd0)](_0x490774,{'image':{'url':_0x2fa577},'caption':_0x5b883a},{'quoted':_0x59764});}catch(_0x508e90){console[_0x13b319(0xd9)](_0x508e90),_0x5968f1(''+_0x508e90);}});
+const {
+  cmd,
+  commands
+} = require('../command');
+const axios = require("axios");
+const cheerio = require('cheerio');
+cmd({
+  'pattern': "cineinfo",
+  'desc': "cinesubz.co info",
+  'category': "search",
+  'filename': __filename
+}, async (_0x4b868b, _0x27181f, _0x3d2b26, {
+  from: _0x30cfa2,
+  quoted: _0x291386,
+  body: _0x4a0a1,
+  isCmd: _0x14e44a,
+  command: _0x348bb9,
+  args: _0x1f5bb3,
+  q: _0xbe85b9,
+  isGroup: _0x11e20b,
+  sender: _0x577c3d,
+  senderNumber: _0x55eee4,
+  botNumber2: _0x202ce5,
+  botNumber: _0x378d91,
+  pushname: _0x2db482,
+  isMe: _0x1e9e0e,
+  isOwner: _0x1185e1,
+  groupMetadata: _0x339a00,
+  groupName: _0x4241ee,
+  participants: _0x415914,
+  groupAdmins: _0x1992ad,
+  isBotAdmins: _0x5dfa80,
+  isAdmins: _0x547368,
+  reply: _0x2d01f2
+}) => {
+  try {
+    if (!_0xbe85b9) {
+      return _0x2d01f2("*Please give me a movie name.‼*");
+    }
+    let _0x58d0cb = await axios.get("https://cinesubz.co/?s=" + _0xbe85b9);
+    let _0x46ed5b = cheerio.load(_0x58d0cb.data);
+    let _0x381e26 = _0x46ed5b("#contenedor > div.module > div.content.rigth.csearch > div > div:nth-child(2) > article > div.details > div.title > a").attr("href");
+    if (!_0x381e26) {
+      let _0x2983f2 = _0x46ed5b("#contenedor > div.module > div.content.rigth.csearch > div > div.no-result.animation-2 > h2 > span").text();
+      return _0x2d01f2("No results to show with *" + _0x2983f2 + '*');
+    }
+    let _0x2be399 = await axios.get('' + _0x381e26);
+    _0x46ed5b = cheerio.load(_0x2be399.data);
+    const _0x53e791 = _0x46ed5b("#single > div.content.right > div.sheader > div.data > h1").text();
+    const _0x4059b4 = _0x46ed5b("#single > div.content.right > div.sheader > div.data > div.extra > span.date").text();
+    const _0x565f1e = _0x46ed5b("#single > div.content.right > div.sheader > div.data > div.extra > span.country").text();
+    const _0x3f9ec6 = _0x46ed5b("#single > div.content.right > div.sheader > div.data > div.extra > span.runtime").text();
+    const _0x5da131 = _0x46ed5b("#repimdb > strong").text();
+    const _0x1ff39 = _0x46ed5b("#cast > div:nth-child(2) > div > div.data > div.name > a").text();
+    const _0x2a9b6e = _0x46ed5b("#single > div.content.right > div.sheader > div.poster > img").attr("src");
+    let _0x4b39b2 = "🍟 *" + _0x53e791 + "*\n\n🧿 *Rᴇʟᴇᴀꜱᴇ Dᴀᴛᴇ :* " + _0x4059b4 + "\n\n🌍 *Cᴏᴜɴᴛʀʏ :* " + _0x565f1e + "\n\n⏱ *Dᴜʀᴀᴛɪᴏɴ :* " + _0x3f9ec6 + "\n\n⭐ *IMDB Rᴀᴛᴇ :* " + _0x5da131 + "\n\n🤵‍♂ *Dɪʀᴇᴄᴛᴏʀ:* " + _0x1ff39 + "\n\n🖇 *Lɪɴᴋ :* " + _0x381e26 + "\n\n> *✨ Powered By SUHAS-MD-V9 💞*";
+    await _0x4b868b.sendMessage(_0x30cfa2, {
+      'image': {
+        'url': _0x2a9b6e
+      },
+      'caption': _0x4b39b2
+    }, {
+      'quoted': _0x27181f
+    });
+  } catch (_0x401b65) {
+    console.log(_0x401b65);
+    _0x2d01f2('' + _0x401b65);
+  }
+});
+cmd({
+  'pattern': "sinhalasublk",
+  'alias': ["cineinfo"],
+  'desc': "Search movies in sinhalasub.lk.",
+  'category': 'search',
+  'filename': __filename
+}, async (_0x1e4285, _0x59764, _0x3d0715, {
+  from: _0x490774,
+  quoted: _0x24942a,
+  body: _0x3a0b67,
+  isCmd: _0x373899,
+  command: _0x272331,
+  args: _0x116239,
+  q: _0x40aea5,
+  isGroup: _0x5567ef,
+  sender: _0x43728b,
+  senderNumber: _0x3a2f92,
+  botNumber2: _0x4a93b5,
+  botNumber: _0x5381ce,
+  pushname: _0x599c57,
+  isMe: _0x1c7961,
+  isOwner: _0x39356b,
+  groupMetadata: _0x1eb995,
+  groupName: _0x684a6d,
+  participants: _0x275be9,
+  groupAdmins: _0x48a970,
+  isBotAdmins: _0x5287e6,
+  isAdmins: _0x2b0af0,
+  reply: _0x5968f1
+}) => {
+  try {
+    const _0x3ad581 = 'https://sinhalasub.lk/?s=' + _0x40aea5;
+    const _0x2b0db0 = await axios.get(_0x3ad581);
+    const _0x1462ac = cheerio.load(_0x2b0db0.data);
+    const _0x4b42b6 = _0x1462ac("#contenedor > div.module > div.content.rigth.csearch > div > div.no-result.animation-2 > h2").text();
+    if (_0x4b42b6 == "No results to show with " + _0x40aea5) {
+      await _0x3d0715.react('❌');
+      return _0x5968f1("*❗️ Mᴏᴠɪᴇ Nᴏᴛ Fᴏᴜɴᴅ. ❗️*");
+    }
+    await _0x3d0715.react('🎬');
+    const _0x5378f0 = [];
+    const _0x47c8d4 = _0x1462ac("article");
+    _0x47c8d4.each(function () {
+      const _0x35d9d3 = _0x1462ac(this).find(".title a").text();
+      const _0x5c5726 = _0x1462ac(this).find('.rating').text();
+      const _0x2b88df = _0x1462ac(this).find(".year").text();
+      const _0x7fe78d = _0x1462ac(this).find(".title a").attr("href");
+      _0x5378f0.push({
+        'title': _0x35d9d3,
+        'rating': _0x5c5726,
+        'year': _0x2b88df,
+        'link': _0x7fe78d
+      });
+    });
+    const _0x47c74b = _0x5378f0[0x0].link;
+    const _0x5468d6 = await axios.get(_0x47c74b);
+    const _0x5ed00f = cheerio.load(_0x5468d6.data);
+    const _0x269df9 = _0x5ed00f("tr:nth-child(1) > td:nth-child(1) > a").attr('href');
+    const _0x5bc606 = _0x5ed00f("tr:nth-child(1) > td:nth-child(3)").text();
+    const _0x11a967 = _0x5bc606.split(" ")[0x0];
+    const _0x3a8d5f = _0x5bc606.search('MB');
+    const _0x343e78 = _0x5ed00f("tr:nth-child(2) > td:nth-child(1) > a").attr("href");
+    const _0x47dd11 = _0x5ed00f("tr:nth-child(2) > td:nth-child(3)").text();
+    const _0xcb2c73 = _0x47dd11.split(" ")[0x0];
+    const _0x5694ce = _0x47dd11.search('MB');
+    const _0x84a4d4 = _0x5ed00f("tr:nth-child(3) > td:nth-child(1) > a").attr("href");
+    const _0xb675bd = _0x5ed00f("tr:nth-child(3) > td:nth-child(3)").text();
+    const _0xaf6037 = _0xb675bd.split(" ")[0x0];
+    const _0x1b4d77 = _0xb675bd.search('MB');
+    const _0x2fa577 = _0x5ed00f("#single > div.content.right > div.sheader > div.poster > img").attr("src");
+    let _0x18b33c = 'MB';
+    let _0x3e8459 = 'MB';
+    let _0x30d3f2 = 'MB';
+    if (_0x3a8d5f == -0x1) {
+      _0x18b33c = 'GB';
+    }
+    if (_0x5694ce == -0x1) {
+      _0x3e8459 = 'GB';
+    }
+    if (_0x1b4d77 == -0x1) {
+      _0x30d3f2 = 'GB';
+    }
+    let _0x5b883a = "\n*📝 Tɪᴛʟᴇ :* " + _0x5378f0[0x0].title + "\n*⭐️ Rᴀᴛɪɴɢ :* " + _0x5378f0[0x0].rating + "\n*📆 Yᴇᴀʀ :* " + _0x5378f0[0x0].year + "\n*🔗 Uʀʟ :* " + _0x5378f0[0x0].link + "\n\n*📥 Download Links 📥*\n*1️⃣ 1080P(" + _0x11a967 + _0x18b33c + ") :*\n" + _0x269df9 + "\n\n*2️⃣ 720P(" + _0xcb2c73 + _0x3e8459 + ") :*\n" + _0x343e78 + "\n\n*3️⃣ 480P(" + _0xaf6037 + _0x30d3f2 + ") :*\n" + _0x84a4d4 + "\n\n*4️⃣ Sɪɴʜᴀʟᴀ Sᴜʙᴛɪᴛʟᴇᴀ :*\n\n> *✨ Powered By SUHAS-MD-V9 💞*";
+    await _0x1e4285.sendMessage(_0x490774, {
+      'image': {
+        'url': _0x2fa577
+      },
+      'caption': _0x5b883a
+    }, {
+      'quoted': _0x59764
+    });
+  } catch (_0x508e90) {
+    console.log(_0x508e90);
+    _0x5968f1('' + _0x508e90);
+  }
+});
